f750def0f69b78a863185995d56c03a0ce159647
Batdarg <132946988+Batdarg@users.noreply.github.com>
2023-09-20 23:18:21 -0600

    Delete AppWeb/8.-Practica presentación PHP directory

--- AppWeb/8.-Practica presentación PHP/tabla.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE if not exists employees (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    address VARCHAR(255) NOT NULL,
-    salary INT(10) NOT NULL
-);
